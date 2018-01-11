c4977aec9b5fc7e307049ae5bea171be4c115887
jtbeckha <jtbeckha@gmail.com>
2018-01-11 18:57:52 -0500

    Solve exercise 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,22 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import iv_properties.toMillis
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+
+    override fun compareTo(other: MyDate): Int {
+        if (this.toMillis() > other.toMillis()) {
+            return 1
+        }
+
+        if (this.toMillis() < other.toMillis()) {
+            return -1
+        }
+
+        return 0
+    }
+
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
