adc5b90453154bb3d5963b2fa270ac6a812c302d
jtbeckha <jtbeckha@gmail.com>
2018-01-13 15:19:28 -0500

    Solve exercise 26

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -26,4 +26,4 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>
